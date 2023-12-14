4f9524b4d90255464550738c47bce558c08327a3
John <128350391+alpaOo@users.noreply.github.com>
2023-12-14 20:16:14 +0800

    Minor changes

--- DS/TEST/tests/trie-tree.test.ts
+++ DS/TEST/tests/trie-tree.test.ts
@@ -1,30 +1,25 @@
 import { describe, test, expect } from "@jest/globals";
-import Trie from "../trie-tree";
+import Trie from "../../trie/src/trie-tree";
 
 describe("Trie module", () => {
-    test("trie", () => {
-        const trie = new Trie();
-        trie.Insert("foo");
-        trie.Insert("fool");
-        trie.Insert("foolish");
-        trie.Insert("food");
-        trie.Insert("zebra");
+  test("trie", () => {
+    const trie = new Trie();
+    trie.Insert("foo");
+    trie.Insert("fool");
+    trie.Insert("foolish");
+    trie.Insert("food");
+    trie.Insert("zebra");
 
-        expect(trie.Find("f").sort()).toEqual([
-            "foo",
-            "food",
-            "fool",
-            "foolish",
-        ]);
-        expect(trie.Find("z")).toEqual(["zebra"]);
+    expect(trie.Find("f").sort()).toEqual(["foo", "food", "fool", "foolish"]);
+    expect(trie.Find("z")).toEqual(["zebra"]);
 
-        expect(trie.Find("fool").sort()).toEqual(["fool"]);
+    expect(trie.Find("fool").sort()).toEqual(["fool"]);
 
-        trie.Insert("bar");
-        expect(trie.Find("b")).toEqual(["bar"]);
+    trie.Insert("bar");
+    expect(trie.Find("b")).toEqual(["bar"]);
 
-        trie.Delete("fool");
+    trie.Delete("fool");
 
-        expect(trie.Find("fo").sort()).toEqual(["foo", "food", "foolish"]);
-    });
+    expect(trie.Find("fo").sort()).toEqual(["foo", "food", "foolish"]);
+  });
 });
